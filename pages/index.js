d517fbc32410996f1894f2bd364d09052bb9b426
Superpol@ris <sallamy2580@gmail.com>
2021-08-25 14:49:15 -0400

    hello frb

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hi <a href="https://nextjs.org">Next.js!</a>
+          Hello FRB <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
