f5cc5e9edb67c4329f722d4bd509fc0bc5ad3e7b
Superpol@ris <sallamy2580@gmail.com>
2021-09-29 19:53:19 -0400

    changed to hello world

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello FRB <a href="https://nextjs.org">Next.js!</a>
+          Hello World! <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
